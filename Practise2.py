b801404b1e7c9adc03cdf1b99b1033ae9c57b5fb
Alialfadhli <134432687+Alialfadhli@users.noreply.github.com>
2023-05-25 16:26:20 +0300

    Delete Practise2.py

--- Practise2.py
+++
@@ -1,8 +0,0 @@
-import csv
-respected = {"FirstName":"Mohammed","LastName":"Almeer","TelephoneNumber":"32379622"}
-
-with open("DataStorage.csv",'w') as f:
-
-fNames=["first Name","Last Name"]
-
-    writer.writeheader()

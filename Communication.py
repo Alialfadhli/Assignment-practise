8c4a9d69c8d5647434789ed8032a4c4497b64652
Alialfadhli <alialfadhli2001@gmail.com>
2023-05-25 16:18:53 +0300

    First Commit

--- Communication.py
+++ Communication.py
@@ -5,5 +5,5 @@ msg = input("Send")
 cs.sendto(msg.encode('ascii'),('127.0.0.1',49999))
 data, address = cs.recvfrom(2048)
 print('Received: ', data.decode('ascii'))
-Yazeed 3mk
+Ali Yazeed 
 cs.close
